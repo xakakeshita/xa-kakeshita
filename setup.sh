3cb5bd0015ca9c063bbe93c76e3ea2a081e12353
xakakeshita <42161592+xakakeshita@users.noreply.github.com>
2019-01-15 12:36:21 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+cd $(dirname ${BASH_SOURCE:-$0})
+
+# 設定
+URL="http://flapsplan.local:8080"
+PUBLIC_DIR="public_html"
+WP_DIR=“/wp-xa-kakeshita "
+WP_DIR="/wp-xa-kakeshita "
+WP_CONTENT_DIR="/wp-content"
+THEME_NAME=“xa-kakeshita”
+DB_NAME=“xa-kakeshita_wp”
+THEME_NAME="xa-kakeshita"
+DB_NAME="xa-kakeshita_wp"
+DB_USER=root
+DB_PASS=password
+DB_HOST=localhost
